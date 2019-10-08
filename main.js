02bce7babe26508fbce989135a7386e8aa8db8b5
Galina <fomina@ndflka.me>
2019-10-08 12:39:19 +0300

    add slider

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ window.addEventListener('DOMContentLoaded', function(){
     });
 
 //Timer
-    let deadline = '2019-09-24';
+    let deadline = '2019-10-24';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -83,7 +83,6 @@ let more = document.querySelector('.more'),
     
 more.addEventListener('click', function() {
     overlay.style.display = 'block';
-    this.classList.add('more-splash');//добавление класса с анимацией
     document.body.style.overflow = "hidden";
 })
 
@@ -95,9 +94,54 @@ close.addEventListener ('click', function(){
 for (let a = 0; a < description.length; a++) {
     description[a].addEventListener('click', function(){
         overlay.style.display = 'block';
-        this.classList.add('more-splash');//добавление класса с анимацией
         document.body.style.overflow = "hidden";
     })
 }
 
+//слайдер
+
+let slideIndex = 1,
+    slides = document.querySelectorAll('.slider-item'),
+    prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.querySelectorAll('.dot');
+
+showSlides(slideIndex);
+function showSlides(n){
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => item.style.display = 'none');
+    // for (let i = 0; i < slides.length; i++) {
+    //     slides[i].style.display = 'none';
+    // }
+    slides[slideIndex - 1].style.display = 'block';
+
+    dots.forEach((item) => item.classList.remove('dot-active'));
+    dots[slideIndex - 1].classList.add('dot-active');
+}
+ 
+function plusSlides (n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlides(n) {
+    showSlides(slideIndex = n); 
+}
+
+prev.addEventListener('click', function() {
+    plusSlides(-1);
 });
+
+next.addEventListener('click', function() {
+    plusSlides(1);
+});
+
+});
+
